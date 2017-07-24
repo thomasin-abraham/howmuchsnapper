1b3bcf070c984f6c7fbcf099a43c8a84ffcb1eff
Thomasin Abraham <thomasinabraham@me.com>
2017-07-24 13:44:21 +1200

    removed connection var from server

--- server/index.js
+++ server/index.js
@@ -1,10 +1,10 @@
 var createServer = require('./server')
-// 
+//
 // var environment = process.env.NODE_ENV || 'development'
 // var config = require('../knexfile')[environment]
 // var connection = require('knex')(config)
 
-var server = createServer(connection)
+var server = createServer()
 
 var PORT = process.env.PORT || 3000
 
