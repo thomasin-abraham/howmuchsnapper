fb1c0fc2ec733e5dc71963d45ec198ef5ee489b8
Thomasin Abraham <thomasinabraham@me.com>
2017-08-06 13:04:19 +1200

    Credits added

--- client/components/Footer.jsx
+++ client/components/Footer.jsx
@@ -5,7 +5,9 @@ import jump from 'jump.js'
 const Footer = () => {
   return (
     <div className="container-fluid text-center">
-      ♡
+      <span className="jam jam-heart"></span><br />
+      <a href="https://www.snapper.co.nz/" title="Snapper website" target="_blank">Thank you Snapper</a><br />
+      <a href="http://jam-icons.com" title="Jam icons website" target="_blank">Icons by Jam Icons</a>
     </div>
   )
 }
