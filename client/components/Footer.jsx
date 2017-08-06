9e7095375fba35a53d795f1071ede3b75b556b4d
Thomasin Abraham <thomasinabraham@me.com>
2017-08-06 18:38:20 +1200

    Add github link

--- client/components/Footer.jsx
+++ client/components/Footer.jsx
@@ -4,8 +4,9 @@ import jump from 'jump.js'
 
 const Footer = () => {
   return (
-    <div className="container-fluid text-center">
+    <div className="container-fluid text-center footer">
       <span className="jam jam-heart"></span><br />
+      <a href="https://github.com/thomasin-abraham/howmuchsnapper"><span className="jam jam-github"></span></a><br />
       <a href="https://www.snapper.co.nz/" title="Snapper website" target="_blank">Thank you Snapper</a><br />
       <a href="http://jam-icons.com" title="Jam icons website" target="_blank">Icons by Jam Icons</a>
     </div>
